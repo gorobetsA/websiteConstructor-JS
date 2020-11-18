f9bd353489b6bd5538aa491fdc902fd4a5362fc6
gorobetsA <andreygorobez@gmail.com>
2020-11-18 15:17:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# websiteConstructor-JS
---
+++ src/model.js
@@ -0,0 +1,20 @@
+import image from './assets/image.png'
+
+export const model = [
+    {type: 'title', value: 'Контструктор сайтов на чистом JavaScript', options: {
+        tag: 'h2',
+            styles: {
+                background: 'linear-gradient(to right, #ff0099, #493240)',
+                color: '#fff',
+                padding: '1.5rem',
+                'text-align': 'center'
+            }
+        }},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+            '1111111111',
+            '2222222222',
+            '3333333333'
+        ] },
+    {type: 'image', value: image}
+]
