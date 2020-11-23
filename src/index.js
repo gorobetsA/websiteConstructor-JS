1503d425f7df53b3a9fddd2d1ef7cb9b5f9e75f0
gorobetsA <andreygorobez@gmail.com>
2020-11-23 10:47:25 +0200

    add some changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import {model} from './model'
-import {templates} from './templates'
+import {App} from './classes/app'
 import './styles/main.css'
 
-const $site = document.querySelector('#site')
-
-
-model.forEach(block => {
-    const toHTML = templates[block.type]
-
-    $site.insertAdjacentHTML('beforeend', toHTML(block))
-})
-
+new App(model).init()
--- src/model.js
+++ src/model.js
@@ -1,20 +1,48 @@
 import image from './assets/image.png'
+import {TextBlock, TitleBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
+
+const text = `
+Конктструктор сайтов на Javasript
+`
 
 export const model = [
-    {type: 'title', value: 'Контструктор сайтов на чистом JavaScript', options: {
-        tag: 'h2',
-            styles: {
-                background: 'linear-gradient(to right, #ff0099, #493240)',
-                color: '#fff',
-                padding: '1.5rem',
-                'text-align': 'center'
-            }
-        }},
-    {type: 'text', value: 'here we go with some text'},
-    {type: 'columns', value: [
-            '1111111111',
-            '2222222222',
-            '3333333333'
-        ] },
-    {type: 'image', value: image}
+  new TitleBlock('Конструктор сайтов на чистом JavaScript', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: 'green',
+      padding: '1.5rem',
+      'text-align': 'center'
+    }
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'Это картинка'
+  }),
+  new ColumnsBlock([
+    'Приложение на чистом JavaScript, без использования библиотек',
+
+  ], {
+    styles: {
+      background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+      padding: '2rem',
+      color: '#fff',
+      'font-weight': 'bold'
+    }
+  }),
+  new TextBlock(text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+  })
 ]
